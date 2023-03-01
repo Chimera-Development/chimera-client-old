5de50d61d13b293ff2616416b1ff194de0821b92
Shrecknt <58538423+Shrecknt@users.noreply.github.com>
2023-03-01 13:31:10 -0800

    just a test

--- src/main/java/dev/chimera/ExampleMod.java
+++ src/main/java/dev/chimera/ExampleMod.java
@@ -6,6 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ExampleMod implements ModInitializer {
+	// Patsore <3s furry femboys
+
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
