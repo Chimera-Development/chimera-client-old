11839dc04d5b2a3fb9914555e248f93caaae9497
Ostap <ostapo.semenikh@gmail.com>
2023-03-01 22:33:25 +0100

    start

--- src/main/java/dev/chimera/ChimeraClient.java
+++ src/main/java/dev/chimera/ChimeraClient.java
@@ -5,9 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
-	// Patsore <3s furry femboys
-
+public class ChimeraClient implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
@@ -19,6 +17,6 @@ public class ExampleMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Hello Chimera sussers!");
 	}
 }
