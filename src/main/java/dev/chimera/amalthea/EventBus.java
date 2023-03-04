cf791dc900bf5223b388156999070c040b7bd73f
Ostap <ostapo.semenikh@gmail.com>
2023-03-04 19:47:08 +0100

    miscellaneous changes

--- src/main/java/dev/chimera/amalthea/EventBus.java
+++ src/main/java/dev/chimera/amalthea/EventBus.java
@@ -1,36 +1,34 @@
 package dev.chimera.amalthea;
 
+import io.wispforest.owo.Owo;
+
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class EventBus implements EventBusInterface {
+public class EventBus{
 
     /*
-      TODO Add Object and method map to also make it faster (instead of compound Listener type
       TODO Also maybe come up with a better way of registering listeners and change the way exceptions are thrown
-      TODO Also make listener registration work in a similar way to mixin registration
     */
 
     public HashMap<TaggedType, List<Method>> taggedListeners = new HashMap<>();
     public HashMap<Class<?>, List<Method>> listeners = new HashMap<>();
-
     public HashMap<Method, Object> methodObjectHashMap = new HashMap<>();
 
     public record TaggedType(String tag, Class<?> klass) {
     }
 
-
-    public void registerListenersInClass(Object object) throws IllegalAccessException {
+    public void registerListenersInClass(Object object){
         for (Method method : object.getClass().getDeclaredMethods()) {
             if (method.isAnnotationPresent(EventListener.class)) {
 
                 String tag = method.getAnnotation(EventListener.class).tag();
-                if(tag.equals("")){
+                if (tag.equals("")) {
                     registerListener(method);
-                }else{
+                } else {
                     registerTaggedListener(method, tag);
                 }
 
@@ -39,8 +37,7 @@ public class EventBus implements EventBusInterface {
         }
 
     }
-
-    public void registerTaggedListener(Method listener, String tag){
+    public void registerTaggedListener(Method listener, String tag) {
         Class<?>[] parameterTypes = listener.getParameterTypes();
         if (parameterTypes.length != 1) {
             throw new IllegalArgumentException("Invalid listener method: " + listener);
@@ -57,7 +54,6 @@ public class EventBus implements EventBusInterface {
         }
         this.taggedListeners.get(taggedType).add(listener);
     }
-
     public void registerListener(Method listener) {
         Class<?>[] parameterTypes = listener.getParameterTypes();
         if (parameterTypes.length != 1) {
@@ -74,8 +70,6 @@ public class EventBus implements EventBusInterface {
         }
         this.listeners.get(klass).add(listener);
     }
-
-    @Override
     public <T> void post(T event) {
         List<Method> methods = listeners.get(event.getClass());
         if (methods != null) {
@@ -88,8 +82,6 @@ public class EventBus implements EventBusInterface {
             }
         }
     }
-
-    @Override
     public <T> void post(String tag, T event) {
         TaggedType taggedType = new TaggedType(tag, event.getClass());
         List<Method> methods = taggedListeners.get(taggedType);
