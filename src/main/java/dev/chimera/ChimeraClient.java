dfa21b490d9f2e4fa650e6ef8ce58747889f768e
Shrecknt <58538423+Shrecknt@users.noreply.github.com>
2023-03-04 15:26:42 -0800

    Call to module initializer class + events

--- src/main/java/dev/chimera/ChimeraClient.java
+++ src/main/java/dev/chimera/ChimeraClient.java
@@ -2,8 +2,11 @@ package dev.chimera;
 
 import dev.chimera.amalthea.EventBus;
 import dev.chimera.amalthea.events.EventSystemTest;
+import dev.chimera.amalthea.events.misc.TickEvent;
+import dev.chimera.modules.ModuleInitializer;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
 import net.fabricmc.fabric.api.event.Event;
 import org.slf4j.Logger;
@@ -25,6 +28,16 @@ public class ChimeraClient implements ModInitializer {
 
 		EVENT_BUS.post("string test");
 		EVENT_BUS.post("sussy", "Works!!");
+
+		new ModuleInitializer().initializeModules();
+
+		ClientTickEvents.START_CLIENT_TICK.register((startTick) -> {
+			EVENT_BUS.post("start", new TickEvent());
+		});
+
+		ClientTickEvents.END_CLIENT_TICK.register((endTick) -> {
+			EVENT_BUS.post("end", new TickEvent());
+		});
 
 	}
 
