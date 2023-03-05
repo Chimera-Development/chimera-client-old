c208ec433c455978d9ba04d479853644ee964559
Shrecknt <58538423+Shrecknt@users.noreply.github.com>
2023-03-04 18:05:37 -0800

    Added key bindings

--- src/main/java/dev/chimera/modules/Module.java
+++ src/main/java/dev/chimera/modules/Module.java
@@ -1,19 +1,40 @@
 package dev.chimera.modules;
 
+import dev.chimera.ChimeraClient;
 import dev.chimera.amalthea.events.misc.TickEvent;
 
 public abstract class Module {
     private final String MODULE_NAME;
     private boolean MODULE_ENABLED;
-    
-    public Module(String name) {
+    private String KEY_BINDING;
+    public boolean releaseToToggle;
+
+    public Module(String name, String bind, boolean toggleOnRelease) {
         MODULE_NAME = name;
         MODULE_ENABLED = false;
+        KEY_BINDING = bind;
+        releaseToToggle = toggleOnRelease;
     }
+    public Module(String name, String bind) {
+        this(name, bind, false);
+    }
+    public Module(String name, boolean toggleOnRelease) {
+        this(name, "-1", toggleOnRelease);
+    }
+    public Module(String name) {
+        this(name, "-1", false);
+    }
+
 
     public String getModuleName() { return MODULE_NAME; }
 
     public boolean getModuleEnabled() { return MODULE_ENABLED; }
+
+    public String getKeyBinding() { return KEY_BINDING; }
+    public void setKeyBinding(String bind) { KEY_BINDING = bind; }
+    public boolean keyBindingMatches(String compareBind) {
+        return KEY_BINDING.equals(compareBind);
+    }
 
     public void setModuleState(boolean state) {
         if (state != MODULE_ENABLED) {
@@ -22,6 +43,8 @@ public abstract class Module {
         }
         MODULE_ENABLED = state;
     }
+
+    public void toggle() { setModuleState(!getModuleEnabled()); }
 
     public abstract void init();
     public abstract void onEnable();
